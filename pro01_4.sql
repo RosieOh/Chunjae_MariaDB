d62e64758fbaf80432fc50de15f60ce189fe0c98
RosieOh <20172207@gm.hannam.ac.kr>
2023-08-10 17:29:10 +0900

    암호화/복호화 비밀번호 암호화 DB 구성

--- pro01_4.sql
+++ pro01_4.sql
@@ -19,3 +19,15 @@ INSERT INTO faq(question, answer) VALUES('자주 묻는 질문입니다4', '답�
 INSERT INTO faq(question, answer) VALUES('자주 묻는 질문입니다5', '답변입니다5');
 
 
+-- 암호화 시키기
+SELECT * FROM member;
+
+DESC member;
+
+-- 고냥 비밀번호 풀려면 개씨발 어렵게 만들어 보리기
+UPDATE member SET pw='03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4' WHERE pw='1234';
+UPDATE member SET pw='fe2592b42a727e977f055947385b709cc82b16b9a87f88c6abf3900d65d0cdc3' WHERE pw='4321';
+UPDATE member SET pw='318aee3fed8c9d040d35a7fc1fa776fb31303833aa2de885354ddf3d44d8fb69' WHERE pw='3333';
+UPDATE member SET pw='0ffe1abd1a08215353c233d6e009613e95eec4253832a761af28ff37ac5a150c' WHERE pw='1111';
+
+COMMIT;
